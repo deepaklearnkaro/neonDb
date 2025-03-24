8a40a61d23de3fcabe28f29652700cdb83bec350
deepaklearnkaro <learnkarodeepak@gmail.com>
2025-03-24 16:21:50 +0530

    setup neondb

---
+++ neon.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Person(BaseModel):
+    name: str
+    surname: str
+    age: int
+
+
+people = []
+
+@app.post("/add-person")
+def add_person(person: Person):
+    people.append(person)
+    return {"message": "Person added successfully", "data": person}
+
+@app.get("/people")
+def get_people():
+    return {"people": people}
