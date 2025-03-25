df91c8a58b6d985f511388d48c3ffd8b5911339d
deepaklearnkaro <learnkarodeepak@gmail.com>
2025-03-25 11:06:35 +0530

    update file name

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/deepak/Desktop/NeonDatabase/venv
+set -gx VIRTUAL_ENV /home/deepak/Desktop/neonDb/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
